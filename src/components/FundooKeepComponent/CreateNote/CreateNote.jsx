66fde0bc73ba3ef2756fb8e5b40c9b3ccbe7ffcc
Shalini8 <shalu8mar@gmail.com>
2021-07-26 13:08:00 +0530

    [Add] IconButton component

--- src/components/FundooKeepComponent/CreateNote/CreateNote.jsx
+++ src/components/FundooKeepComponent/CreateNote/CreateNote.jsx
@@ -1,48 +1,63 @@
-import * as React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Grid, TextField } from '@material-ui/core';
-import CheckBoxOutlinedIcon from '@material-ui/icons/CheckBoxOutlined';
-import BrushIcon from '@material-ui/icons/Brush';
-import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
+import * as React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Grid, TextField } from "@material-ui/core";
+import CheckBoxOutlinedIcon from "@material-ui/icons/CheckBoxOutlined";
+import BrushIcon from "@material-ui/icons/Brush";
+import ImageOutlinedIcon from "@material-ui/icons/ImageOutlined";
 import "../CreateNote/CreateNote.css";
+import IconButton from "../IconButton/IconButton";
 
-export default class  CreateNote extends React.Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             
-        }
-    }
-    
-    render() {
-        return (
-            <div className='card-container'>
-                
-         <Card className='input-card card-shadow' variant="outlined">
+export default class CreateNote extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="card-container">
+        <Card className="input-card card-shadow" variant="outlined">
+          <CardContent>
+            <Grid>
+              <input
+                className="title"
+                type="text"
+                placeholder="Take a Note"
+              ></input>
+              <span className="takenote-icons">
+                <CheckBoxOutlinedIcon className="check-icon" />
+                <BrushIcon className="brush-icon" />
+                <ImageOutlinedIcon className="image-icon" />
+              </span>
+            </Grid>
+
+          </CardContent>
+         
+        </Card>
+        {/* <div>
+        <Card className="input-card card-shadow" variant="outlined">
         <CardContent>
-          <Grid >
-        <input className="title" type = "text" placeholder ='Take a Note'></input>
-        <span className='takenote-icons'>
-        <CheckBoxOutlinedIcon className='check-icon'/>
-        <BrushIcon className='brush-icon'/>
-        <ImageOutlinedIcon className='image-icon'/>
-        </span>
-        </Grid>
-      
-        {/* <textarea className="title" placeholder='title' ></textarea> */}
-      </CardContent>
-      {/* <CardActions>
-        <Button size="small">Close</Button>
-      </CardActions> */}
-    </Card>
-  
 
-            </div>
-        );
-    }
+         <textarea className="title" placeholder='Title' ></textarea> <br></br>
+         <textarea className="title" placeholder='Take a Note' ></textarea> 
+
+          </CardContent>
+          <div className ="icon-close">
+            <div className='iconbtn'>
+          <IconButton /> 
+          </div>    
+          <Button size="small">Close</Button>
+          </div>
+         </Card>
+
+        </div> */}
+      </div>
+      
+    );
+  }
 }
---
+++ src/components/FundooKeepComponent/IconButton/IconButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import AddAlertIcon from "@material-ui/icons/AddAlertOutlined";
+import CollaboratorIcon from "@material-ui/icons/PersonAddOutlined";
+import ColorLensIcon from "@material-ui/icons/ColorLensOutlined";
+import ArchiveIcon from "@material-ui/icons/ArchiveOutlined";
+import PhotoIcon from "@material-ui/icons/PhotoOutlined";
+import MoreIcon from "@material-ui/icons/MoreVertOutlined";
+import "../IconButton/IconButton.css"
+
+
+export default function IconButton() {
+    return (
+        <div>
+        <AddAlertIcon className="btn-icon" />
+        <CollaboratorIcon className="btn-icon" />
+        <ColorLensIcon className="btn-icon" />
+        <PhotoIcon className="btn-icon" />
+        <ArchiveIcon className="btn-icon" />
+        <MoreIcon className="btn-icon" />
+    </div>
+    )
+}
